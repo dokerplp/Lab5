ab8ed6455f168a624f6baf1cd6353e77bea39f2c
dokerplp <dokerplp2113@gmail.com>
2021-03-03 23:14:06 +0300

    jar ok, assembly no 2

--- v1.01/src/com/company/CreationException.java
+++
@@ -1,7 +0,0 @@
-package com.company;
-
-public class CreationException extends Exception {
-    public String toString(){
-        return "Ошибка данных";
-    }
-}
--- v1.02/src/com/company/CommandPattern/Command.java
+++
@@ -1,9 +0,0 @@
-package com.company.CommandPattern;
-
-/**
- * This interface implements all Commands
- */
-public interface Command {
-    void execute(String Argument);
-}
-
